163409f8e03b0478a6e3e02649994c3e7d3e48f3
Tomer27cz <tomer19cz@gmail.com>
2025-02-03 01:54:57 +0100

    some hacs things

--- src/energy-line-gauge.js
+++ src/energy-line-gauge.js
@@ -1,4 +1,4 @@
-import { LitElement, html, css } from "lit";
+import { LitElement, html, css } from "./lit";
 
 import { EnergyLineGaugeEditor } from "./editor.js";
 import { EnergyLineGaugeEditorEntitiesCardRowEditor } from "./entities-card-row-editor.js";
